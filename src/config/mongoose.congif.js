b813889f7cc16f1b66540acff92ae5e2b296cba1
11shazam11 <105714122+11shazam11@users.noreply.github.com>
2024-11-07 12:09:09 +0530

    mongoose.congif.js

--- src/config/mongoose.congif.js
+++ src/config/mongoose.congif.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 // Connecting to MongoDB via Mongoose
 export const connectByMongoose = async () => {
     try {
-        await mongoose.connect(process.env.DB_Url)
+        await mongoose.connect("mongodb+srv://dhumneabhay:N8cbIEByijv2R7SP@cluster0.4juvi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/postaway");
         console.log("Connected to MongoDB via Mongoose");
     } catch (error) {
         console.error("Error connecting to MongoDB:", error.message);
